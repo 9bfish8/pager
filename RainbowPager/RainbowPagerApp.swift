0db1ef36f60ea38334f68d9730626424e6103d46
9bfish8 <9bfish8@gmail.com>
2026-02-11 19:46:12 +0900

    Initial Commit

---
+++ RainbowPager/RainbowPagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  RainbowPagerApp.swift
+//  RainbowPager
+//
+//  Created by 정가연 on 2/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RainbowPagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RainbowPagerTests/RainbowPagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  RainbowPagerTests.swift
+//  RainbowPagerTests
+//
+//  Created by 정가연 on 2/11/26.
+//
+
+import Testing
+
+struct RainbowPagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
